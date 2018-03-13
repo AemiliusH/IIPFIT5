766a7b6a69be145486bf240f1bce3fa81bfd4f8c
Eddie <s1103183@student.hsleiden.nl>
2018-03-13 14:04:04 +0100

    Kleurtjes in console!

--- Utils/log.py
+++ Utils/log.py
@@ -2,10 +2,21 @@ import time
 from datetime import * 
 from tabulate import tabulate
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 class DebugLog():
     def __init__(self,tekst): 
         #timestamp = '{}-{}-{} {}:{}:{}'.format(date.year, date.month, date.day, time.hour, time.minute, time.second)
-        print "[_] " + str(tekst)
+        print bcolors.WARNING + "[_] " + str(tekst) + bcolors.ENDC
 
 class ReportLog():
     def __init__(self, tekst): 
