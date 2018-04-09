236a851b7a7f47049f294e6b49c3f99e4ba20bf0
AemiliusH <s1104099@student.hsleiden.nl>
2018-04-09 13:31:14 +0200

    Database Shit

--- Utils/Database.py
+++ Utils/Database.py
@@ -1,32 +1,141 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, MetaData, Table, DateTime, select
+from sqlalchemy.orm import mapper, sessionmaker, clear_mappers
+from sqlalchemy import Column, Integer
+from tabulate import tabulate
 
-path = '..//DataBase//DB.sqlite'
-engine = create_engine('sqlite:///%s' % path, echo=False)
-Base = declarative_base()
-Session = sessionmaker(bind=engine)
-session = Session()
-
+from Models import *
 
 class Database():
+    userid = 2
+    caseid = -1
     def __init__(self, hoofdmenu):
         self.hoofdmenu_refrentie = hoofdmenu
+        path = 'DataBase//DB.sqlite'  # path = '..//DataBase//DB.sqlite' voor testen vanuit deze class
+        engine = create_engine('sqlite:///%s' % path, echo=False)
+        self.conn = engine.connect()
+        Base.metadata.create_all(bind=engine)
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
+        self.pad = None
+        self.run()
+
+    def select_user(self):
+        #inladen userid voor later gebruik
+        print "Welke gebruiker wilt u kiezen?"
+        for name in self.session.query(User):
+            print name.ID,name.Naam,name.Achternaam
+        self.userid = raw_input("Kies een optie")
+
+
+    def add_user(self, voornaam, achternaam):
+        voornaam = raw_input("Vul uw voornaam in: ")
+        achternaam = raw_input("Vul uw achternaam in: ")
+
+        user = User(Naam=voornaam, Achternaam=achternaam)
+
+        self.session.add(user)
+        self.session.commit()
+
+
+    def select_case(self):
+        # Lees image paden uit met ;
+        # Leest case ID uit
+
+        case = self.session.query(Case)
+        for i in case:
+            print i.ID, i.Naam, i.Image
+        case_nr = int(raw_input("Kies een optie"))
+        self.caseid = case_nr
+        select_st = select([Case]).where(
+            Case.ID == case_nr)
+        selected_case = self.conn.execute(select_st)
+        for row in selected_case:
+            print 'Inladen van ', row.Image
+            self.hoofdmenu_refrentie.add_image(row.Image)
+
+    def select_image(self, path):
+
+
+    def add_case(self, naam):
+
+    def add_image(self, path):
+        # Eerst alle images uitlezen, vervolgens herschrijven
+        # werk met ;;;;   om meerdere images toe te voegen
+
+        # Stap loggen!!
+
+    def write_log(self, log):
+        # Log wegschrijven met UserID en CaseID / Timestamp
+
+
+    def write_rapportage(self, rapport):
+        #meerdere regels??
+        # Wegschrijven met USerid en CaseID
+
+    def write_error(self, error):
+        # same met userid en caseid
+
+    def write_export(self, naam, meta, doel, bron):
+        # wegschrijven emt userid en caseid
 
 
 
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    fullname = Column(String)
-    password = Column(String)
 
-    def __repr__(self):
-        return "<User(name='%s', fullname='%s', password='%s')>" % (self.name, self.fullname, self.password)
+    #def add_image(self, path):
+        #self.write_log('Heeft image toegevoegd aan case')
 
-ed_user = User(name='ed', fullname='Ed Jones', password='edspassword')
+    def cli(self):
+        while True:
+            print "\t[1] Nieuwe Case"
+            print "\t[2] Bestaande Case Kiezen"
+            print "\t[3] Terug"
+
+            optie = int(raw_input("Kies een optie [0-9]"))
+            # date = DateTime()
+
+            if optie == 1:
+                self.add_case()
+
+
+            if optie == 2:
+                self.select_case()
+
+
+            if optie == 3:
+                break
+
+    def add_case(self):
+        image = str(raw_input("Voer het pad naar de image in"))
+        naam = str(raw_input("Voer de naam van de case in: "))
+
+        case = Case(Image=image, Naam=naam)
+
+        self.session.add(case)
+        self.session.commit()
+
+    def select_case(self):
+
+
+
+
+
+
+    def run(self):
+        self.cli()
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ Utils/Models.py
@@ -0,0 +1,127 @@
+
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+'''CREATE TABLE `User` (
+	`ID`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`Naam`	TEXT NOT NULL,
+	`Achternaam`	TEXT NOT NULL
+);'''
+
+class User(Base):
+    __tablename__ = 'User'
+    ID= Column(Integer(), primary_key=True)
+    Naam = Column(String(50))
+    Achternaam = Column(String(50))
+
+'''CREATE TABLE `Case` (
+	`ID`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`Image`	INTEGER NOT NULL,
+	`Naam`	TEXT NOT NULL,
+	`Datum`	INTEGER NOT NULL,
+	`UserID`	INTEGER NOT NULL
+);'''
+
+class Case(Base):
+    __tablename__ = 'Case'
+    ID= Column(Integer(), primary_key=True)
+    Image = Column(Integer())
+    Naam = Column(String(50))
+    #Datum = Column(DateTime())
+
+
+'''CREATE TABLE `Error_Logs` (
+	`ID`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`Melding`	TEXT NOT NULL,
+	`Tijd`	INTEGER NOT NULL,
+	`User ID`	INTEGER NOT NULL,
+	`Case ID`	INTEGER NOT NULL,
+	FOREIGN KEY(`Case ID`) REFERENCES `Case`(`ID`),
+	FOREIGN KEY(`User ID`) REFERENCES `User`(`ID`)
+);'''
+
+class Error_Logs(Base):
+    __tablename__ = 'Error_Logs'
+    ID= Column(Integer(), primary_key=True)
+    Melding = Column(String(50))
+    Tijd = Column(Integer())
+    UserID = Column(Integer(), ForeignKey('User.ID'))
+    CaseID = Column(Integer(), ForeignKey('Case.ID'))
+
+'''CREATE TABLE `Exports` (
+	`ID`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`UserID`	INTEGER NOT NULL,
+	`CaseID`	INTEGER NOT NULL,
+	`Bestandsnaam`	TEXT NOT NULL,
+	`MetaData`	TEXT NOT NULL,
+	`LocatieBron`	TEXT NOT NULL,
+	`LocatieDoel`	TEXT NOT NULL,
+	`Datum`	TEXT NOT NULL,
+	FOREIGN KEY(`CaseID`) REFERENCES `Case`(`ID`),
+	FOREIGN KEY(`UserID`) REFERENCES `User`(`ID`)
+);'''
+
+
+class Exports(Base):
+    __tablename__ = 'Exports'
+    ID= Column(Integer(), primary_key=True)
+    UserID = Column(Integer(), ForeignKey('User.ID'))
+    CaseID = Column(Integer(), ForeignKey('Case.ID'))
+    Bestandsnaam = Column(String(50))
+    MetaData = Column(String(50))
+    LocatieBron = Column(String(50))
+    LocatieDoel = Column(String(50))
+    Datum = Column(Integer())
+
+
+'''CREATE TABLE `Image` (
+	`ID`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`Naam`	TEXT NOT NULL,
+	`Path`	TEXT NOT NULL,
+	`Grootte`	INTEGER NOT NULL
+);'''
+
+class Image(Base):
+    __tablename__ = 'Image'
+    ID= Column(Integer(), primary_key=True)
+    Naam = Column(String(50))
+    Path = Column(String(50))
+    Grootte = Column(String(50))
+
+
+'''CREATE TABLE `Logboek` (
+	`ID`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`UserID`	INTEGER NOT NULL,
+	`CaseID`	INTEGER NOT NULL,
+	`Handeling`	TEXT NOT NULL,
+	`Tijd`	INTEGER NOT NULL,
+	FOREIGN KEY(`CaseID`) REFERENCES `Case`(`ID`),
+	FOREIGN KEY(`UserID`) REFERENCES `User`(`ID`)
+);'''
+
+class Logboek(Base):
+    __tablename__ = 'Logboek'
+    ID= Column(Integer(), primary_key=True)
+    UserID = Column(Integer(), ForeignKey('User.ID'))
+    CaseID = Column(Integer(), ForeignKey('Case.ID'))
+    Handeling = Column(String(50))
+    Tijd = Column(Integer)
+
+'''CREATE TABLE `Rapportage` (
+	`ID`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`UserID`	INTEGER NOT NULL,
+	`CaseID`	INTEGER NOT NULL,
+	`Resultaten`	TEXT NOT NULL,
+	FOREIGN KEY(`UserID`) REFERENCES `User`(`ID`),
+	FOREIGN KEY(`CaseID`) REFERENCES `Case`(`ID`)
+);'''
+
+
+class Rapportage(Base):
+    __tablename__ = 'Rapportage'
+    ID= Column(Integer(), primary_key=True)
+    UserID = Column(Integer(), ForeignKey('User.ID'))
+    CaseID = Column(Integer(), ForeignKey('Case.ID'))
+    Resultaten = Column(String(50))
